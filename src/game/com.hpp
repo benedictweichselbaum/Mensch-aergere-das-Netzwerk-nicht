491b2b6594542909d0c6d25dcdb02ad13a14ac56
Daniel Kraus <51489488+dtiggy@users.noreply.github.com>
2020-01-13 19:24:45 +0100

    Way more intelligent Com2 added

--- src/game/com.hpp
+++ src/game/com.hpp
@@ -2,27 +2,40 @@
 #include <iostream>
 #include <random>
 #include <time.h>
+#include <cstdint>
 
-void Com(int8_t* game,int8_t comnumber);
+struct Com2Struct
+{
+	bool isPossible[5];
+	int8_t progress[5];
+	int8_t canThrow[5];
+	int canBeThrownDirectly[5];
+};
 
-void Com1(int8_t* game,int8_t rolledNumber,int8_t playerNumber);
+void Com(int8_t* game, int8_t comnumber);
 
-int8_t HowManyMeeplesInStartHouse(int8_t* game,int8_t playerNumber);
+void Com1(int8_t* game, int8_t rolledNumber, int8_t playerNumber);
 
-int8_t RollTheDice(int8_t seedHelper);
+void Com2(int8_t* game, int8_t rolledNumber, int8_t playerNumber);
+
+int8_t HowManyMeeplesInStartHouse(int8_t* game, int8_t playerNumber);
+
+int8_t RollTheDice(int8_t seedhelper);
 
 int8_t GetIndexOfStartPoint(int8_t playerNumber);
 
-void MeepleKickedOut(int8_t* game,int8_t player);
+void MeepleKickedOut(int8_t* game, int8_t player);
 
-void HasToPlattenPutzen(int8_t* game,int8_t playerNumber,int8_t rolledNumber);
+void HasToPlattenPutzen(int8_t* game, int8_t playerNumber, int8_t rolledNumber);
 
-int8_t AddDiceAmountToPosition(int8_t position,int8_t dice);
+int8_t AddDiceAmountToPosition(int8_t position, int8_t dice);
 
-int8_t ProgressOfMeeple(int8_t* game,int8_t playerNumber,int8_t meepleNumber);
+int8_t ProgressOfMeeple(int8_t* game, int8_t playerNumber, int8_t meepleNumber);
 
-bool IsMovePossible(int8_t* game,int8_t playerNumber,int8_t progressOfMeeple,int8_t rolledNumber);
+bool IsMovePossible(int8_t* game, int8_t playerNumber, int8_t progressOfMeeple, int8_t rolledNumber);
 
-int8_t GetIndexWithProgress(int8_t playerNumber,int8_t progressOfMeeple);
+int8_t GetIndexWithProgress(int8_t playerNumber, int8_t progressOfMeeple);
 
-void Move(int8_t* game,int8_t playerNumber,int8_t progressOfMeeple,int8_t rolledNumber);
+void Move(int8_t* game, int8_t playerNumber, int8_t progressOfMeeple, int8_t rolledNumber);
+
+int8_t GetIndexOfFieldsBeforeMeepleByProgress(int8_t playerNumber, int8_t progress, int8_t fieldsBackwards);
